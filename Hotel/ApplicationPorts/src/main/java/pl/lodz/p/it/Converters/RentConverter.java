fc76c7f3c1d0b0e9d906c73a1bc5ba3e2cc82601
GabroVV <origingabriel3124@gmail.com>
2020-04-01 00:58:13 +0200

    Wszystkie convertery auuu

---
+++ Hotel/ApplicationPorts/src/main/java/pl/lodz/p/it/Converters/RentConverter.java
@@ -0,0 +1,43 @@
+package pl.lodz.p.it.Converters;
+
+import com.mycompany.store.Model.Client;
+import com.mycompany.store.Model.Rent;
+import com.mycompany.store.Model.Rentable;
+import pl.lodz.p.it.RA.Model.ClientRA;
+import pl.lodz.p.it.RA.Model.RentRA;
+import pl.lodz.p.it.RA.Model.RentableRA;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.Calendar;
+@Named
+@ApplicationScoped
+public class RentConverter {
+
+    @Inject
+    RentableConverter rentableConverter;
+
+    @Inject
+    UserConverter userConverter;
+
+    public RentRA convertToRepository(Rent rent){
+        RentableRA rentableRA = rentableConverter.convertToRepository(rent.getRentable());
+        ClientRA clientRA = (ClientRA) userConverter.convertToRepository(rent.getClient());
+        return new RentRA(
+                rentableRA,
+                clientRA,
+                rent.getRentStart(),
+                rent.getRentStop());
+    }
+
+    public Rent convertToRepository(RentRA rent){
+        Rentable rentable = rentableConverter.convertToDomain(rent.getRentable());
+        Client client = (Client) userConverter.convertToDomain(rent.getClient());
+        return new Rent(
+                rentable,
+                client,
+                rent.getRentStart(),
+                rent.getRentStop());
+    }
+}
