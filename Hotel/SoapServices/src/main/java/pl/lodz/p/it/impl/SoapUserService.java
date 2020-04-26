b0904be8935f39356a21acffe3229c18e6f7c0ec
Jakub Bogdan <kubab1998@o2.pl>
2020-04-26 20:03:20 +0200

    Interface Soap

--- Hotel/SoapServices/src/main/java/pl/lodz/p/it/impl/SoapUserService.java
+++ Hotel/SoapServices/src/main/java/pl/lodz/p/it/impl/SoapUserService.java
@@ -1,67 +1,66 @@
-package pl.lodz.p.it;
+package pl.lodz.p.it.impl;
 
 import pl.lodz.p.it.Aggregates.UserAdapterSoapAndRA;
 import pl.lodz.p.it.SoapModel.AdminSoap;
 import pl.lodz.p.it.SoapModel.ClientSoap;
 import pl.lodz.p.it.SoapModel.ManagerSoap;
 import pl.lodz.p.it.SoapModel.UserSoap;
+import pl.lodz.p.it.SoapUserServiceInterface;
 
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.jws.WebMethod;
-import javax.jws.WebParam;
 import javax.jws.WebService;
 import java.util.Map;
 
 @Named
 @ApplicationScoped
-@WebService
-public class SoapUserService {
+@WebService(endpointInterface = "pl.lodz.p.it.SoapUserServiceInterface")
+public class SoapUserService implements SoapUserServiceInterface {
 
     @Inject
     UserAdapterSoapAndRA userAdapter;
 
     public SoapUserService() { }
 
-    @WebMethod
+    @Override
     public Map<String, UserSoap> getUsers() {
         return userAdapter.getUsers();
     }
 
-    @WebMethod
-    public Map<String, UserSoap> getFilteredUsers(@WebParam String input) {
+    @Override
+    public Map<String, UserSoap> getFilteredUsers(String input) {
         return userAdapter.getFilteredUsers(input);
     }
 
-    @WebMethod
+    @Override
     public Map<String, ClientSoap> getClients() {
         return userAdapter.getClients();
     }
 
-    @WebMethod
-    public UserSoap getUser(@WebParam String login) {
+    @Override
+    public UserSoap getUser(String login) {
         return userAdapter.getUser(login);
     }
 
-    @WebMethod
-    public void addClient(@WebParam ClientSoap client) {
+    @Override
+    public void addClient(ClientSoap client) {
         userAdapter.addClient(client);
     }
 
-    @WebMethod
-    public void addAdmin(@WebParam AdminSoap admin) {
+    @Override
+    public void addAdmin(AdminSoap admin) {
         userAdapter.addAdmin(admin);
     }
 
-    @WebMethod
-    public void addManager(@WebParam ManagerSoap manager) {
+    @Override
+    public void addManager(ManagerSoap manager) {
         userAdapter.addManager(manager);
     }
 
-    @WebMethod
-    public void updateUser(@WebParam UserSoap user) {
+    @Override
+    public void updateUser(UserSoap user) {
         userAdapter.updateUser(user, user.getPassword(), user.getName(), user.getSurname());
     }
 }
--- Hotel/UserInterface/UIControllers/src/main/java/UIControllers/SoapListUsersController.java
+++ Hotel/UserInterface/UIControllers/src/main/java/UIControllers/SoapListUsersController.java
@@ -1,4 +1,18 @@
 package UIControllers;
 
-public class SoapListUsersController {
+import pl.lodz.p.it.SoapUserServiceInterface;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class SoapListUsersController implements Serializable {
+
+    @Inject
+    private SoapUserServiceInterface soapInterface;
+
+    public SoapListUsersController () {}
 }
