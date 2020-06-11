cb98414852b801de44bfd5a66c5ad97d813dd899
Jakub Bogdan <kubab1998@o2.pl>
2020-06-11 15:44:29 +0200

    Jednak bez properties

--- Hotel/MQController/src/main/java/pl/lodz/p/it/Publisher.java
+++ Hotel/MQController/src/main/java/pl/lodz/p/it/Publisher.java
@@ -3,38 +3,40 @@ package pl.lodz.p.it;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import lombok.SneakyThrows;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Properties;
 
 @RequestScoped
 public class Publisher {
 
-    Properties properties = new Properties();
+    private final String HOST_NAME = "localhost";
+    private final String EXCHANGE_NAME = "exchange_topic";
+    private final String EXCHANGE_TYPE = "topic";
+
     private ConnectionFactory connectionFactory;
     private Connection connection;
     private Channel channel;
 
-    @SneakyThrows
     @PostConstruct
     public void init() {
-        properties.load(new FileInputStream("MQController/src/main/resources/rabbit.properties"));
-        connectionFactory = new ConnectionFactory();
-        connectionFactory.setHost(properties.getProperty("host_name"));
-        connection = connectionFactory.newConnection();
-        channel = connection.createChannel();
-        channel.exchangeDeclare(properties.getProperty("exchange_name"), properties.getProperty("exchange_type"));
+        try {
+            connectionFactory = new ConnectionFactory();
+            connectionFactory.setHost(HOST_NAME);
+            connection = connectionFactory.newConnection();
+            channel = connection.createChannel();
+            channel.exchangeDeclare(EXCHANGE_NAME, EXCHANGE_TYPE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void createUser(String role, String login, String name, String surname, boolean isActive, String password) throws IOException {
         StringBuilder object = new StringBuilder();
         object.append(role).append(";").append(login).append(";").append(name).append(";")
                 .append(surname).append(";").append(isActive).append(";").append(password).append(";");
-        channel.basicPublish(properties.getProperty("exchange_name"), "user.create" ,null, object.toString().getBytes(StandardCharsets.UTF_8));
+        channel.basicPublish(EXCHANGE_NAME, "user.create" ,null, object.toString().getBytes(StandardCharsets.UTF_8));
     }
 }
--- UserService/MQController/src/main/java/pl/lodz/p/it/Receiver.java
+++ UserService/MQController/src/main/java/pl/lodz/p/it/Receiver.java
@@ -4,45 +4,45 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
-import lombok.SneakyThrows;
 import lombok.extern.java.Log;
 
 import javax.annotation.PostConstruct;
 
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Properties;
 
 @Startup
 @Singleton
 @Log
 public class Receiver {
 
-    Properties properties = new Properties();
+    private final String HOST_NAME = "localhost";
+    private final String EXCHANGE_NAME = "exchange_topic";
+    private final String EXCHANGE_TYPE = "topic";
+
     private ConnectionFactory connectionFactory;
     private Connection connection;
     private Channel channel;
 
-    @SneakyThrows
     @PostConstruct
     public void init() {
-        properties.load(new FileInputStream("MQController/src/main/resources/rabbit.properties"));
-        connectionFactory = new ConnectionFactory();
-        connectionFactory.setHost(properties.getProperty("host_name"));
-
-        connection = connectionFactory.newConnection();
-        channel = connection.createChannel();
-        channel.exchangeDeclare(properties.getProperty("exchange_name"), properties.getProperty("exchange_type"));
-        createUser();
+        try {
+            connectionFactory = new ConnectionFactory();
+            connectionFactory.setHost(HOST_NAME);
+            connection = connectionFactory.newConnection();
+            channel = connection.createChannel();
+            channel.exchangeDeclare(EXCHANGE_NAME, EXCHANGE_TYPE);
+            createUser();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void createUser() throws IOException {
         String queueName = channel.queueDeclare().getQueue();
-        channel.queueBind(queueName, properties.getProperty("exchange_name"), "user.create");
-
+        channel.queueBind(queueName, EXCHANGE_NAME, "user.create");
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
